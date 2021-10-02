3cdf130316a79404e0c6075dad78e9b9281ab7d6
paimdev <bathkelucas@gmail.com>
2021-10-02 11:38:32 -0300

    Start of the project

---
+++ main.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+
+function Book(author, title, numberOfPages, read, extra) {
+  this.author = author,
+  this.title = title,
+  this.numberOfPages = numberOfPages,
+  this.read = read,
+  this.extra = extra;
+};
+
+function addBookToLibrary() {
+  
+}
