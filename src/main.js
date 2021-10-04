5871f9f85b08de733193feac724b0bebacb4de65
paimdev <bathkelucas@gmail.com>
2021-10-04 08:36:59 -0300

    Fix doubled main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ function Book(author, title, numberOfPages, read, extra) {
   this.author = author,
   this.title = title,
   this.numberOfPages = numberOfPages,
-  this.read = read,
-  this.extra = extra;
+  this.read = read
 };
 
 function addBookToLibrary() {
   
+}
+
+function bookDisplayer(booksArray) {
+  for (const book of booksArray) {
+    
+  }
 }
