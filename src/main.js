f147df91554305bf90cd0dc63da5b7ab75a3a4a3
paimdev <bathkelucas@gmail.com>
2021-10-05 17:38:09 -0300

    Add event listener to the add book button

--- src/main.js
+++ src/main.js
@@ -13,16 +13,19 @@ function addBookToLibrary(author, title, numberOfPages, read) {
 }
 
 const container = document.querySelector(".container");
+const newButton = document.querySelector(".new-book");
+const form = document.querySelector(".form-container");
+
 
 function bookDisplayer(booksArray) {
   for (const book of booksArray) {
     const bookCard = document.createElement("div");
-
+    
     const authorParagraph = document.createElement("p");
     const titleParagraph = document.createElement("p");
     const pagesParagraph = document.createElement("p");
     const readParagraph = document.createElement("p");
-
+    
     authorParagraph.textContent = book.author;
     titleParagraph.textContent = book.title;
     pagesParagraph.textContent = book.numberOfPages;
@@ -32,9 +35,16 @@ function bookDisplayer(booksArray) {
     bookCard.appendChild(titleParagraph);
     bookCard.appendChild(pagesParagraph);
     bookCard.appendChild(readParagraph);
-
+    
     container.appendChild(bookCard);
   }
 }
 
+function createForm() {
+  
+};
+
+addBookToLibrary('some author', 'some title', 'some pages', 'some status');
 bookDisplayer(myLibrary);
+
+newButton.addEventListener('click', createForm());
